279ba9e5bb14db816cf23c6e73ea82eb082949b9
LucentW <LucentW@users.noreply.github.com>
2021-10-10 21:11:27 +0200

    Fix function casting

--- queries.c
+++ queries.c
@@ -2091,7 +2091,7 @@ static void _tgl_do_send_photo (struct tgl_state *TLS, tgl_peer_id_t to_id, cons
       }
     } else {
       if (callback) {
-        ((void (*)(struct tgl_state *, void *, int))callback) (TLS, callback_extra, 0);
+        ((void (*)(struct tgl_state *, void *, int, struct tgl_message *))callback) (TLS, callback_extra, 0, NULL);
       }
     }
     return;
@@ -2108,7 +2108,7 @@ static void _tgl_do_send_photo (struct tgl_state *TLS, tgl_peer_id_t to_id, cons
       }
     } else {
       if (callback) {
-        ((void (*)(struct tgl_state *, void *, int))callback) (TLS, callback_extra, 0);
+        ((void (*)(struct tgl_state *, void *, int, struct tgl_message *))callback) (TLS, callback_extra, 0, NULL);
       }
     }
     return;
@@ -2141,7 +2141,7 @@ static void _tgl_do_send_photo (struct tgl_state *TLS, tgl_peer_id_t to_id, cons
       }
     } else {
       if (callback) {
-        ((void (*)(struct tgl_state *, void *, int))callback) (TLS, callback_extra, 0);
+        ((void (*)(struct tgl_state *, void *, int, struct tgl_message *))callback) (TLS, callback_extra, 0, NULL);
       }
     }
     return;
